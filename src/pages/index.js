a1f0ebf06cd5894fd55da3155285274f56aaaf27
Hrushikesh Walujkar <hrushiwalujkar@gmail.com>
2024-01-09 12:05:34 +0530

    Toolbox UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
+import Toolbox from '@/components/Toolbox'
 import Menu from '../components/Menu/index'
 export default function Home() {
   return (
-    <Menu />
+    <>
+      <Menu />
+      <Toolbox/>
+    </>
  )
 }
